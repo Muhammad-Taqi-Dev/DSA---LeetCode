869a80e7822c28cd67cfcd784f79d80fbb86f987
taqihaider110 <taqihaider591@gmail.com>
2026-04-20 16:31:09 +0500

    longest Common Prefix Solution

---
+++ Strings/Python3/Longest Common Prefix copy.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        check = strs[0]
+        for i in range(len(check)):
+            for word in strs[1:]:
+                
+                if i == len(word) or check[i] != word[i]:
+                    return check[:i]
+
+        return check
